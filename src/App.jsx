2aaf38dbde794da31ea83bea9fb67473d387f194
anirudhkhemriyaa <anirudhakhemriya06@gmail.com>
2026-04-09 23:30:18 +0530

    hh

--- src/App.jsx
+++ src/App.jsx
@@ -24,7 +24,7 @@ export default function App() {
     return () => window.removeEventListener("scroll", handleScroll);
   }, []);
 
-  /
+  
   const toggleMobileMenu = () => {
     setMobileNavbarOpen(prev => !prev);
   };
